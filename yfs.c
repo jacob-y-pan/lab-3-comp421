1e25f9a6ba4b5813ae62a90ea544d8c2454ee2f4
Jacob Pan <supajake118@gmail.com>
2023-04-10 17:42:07 -0500

    update yfs

--- yfs.c
+++ yfs.c
@@ -8,5 +8,16 @@ main(int argc, char **argv)
     (void) argv;
     TracePrintf(0, "Hello");
 
+    // Per page 19, register this server
+    Register(FILE_SERVER);
+
+    // Fork and exec the first user process
+    if (Fork() == 0) {
+        // As child
+        Exec(argv[1], argv + 1);
+    } else {
+
+    }
+
     return 0;
 }
