aea01fbccab48bc01310c01acfad3f8d5ac95515
Jacob Pan <supajake118@gmail.com>
2023-04-10 17:19:16 -0500

    initial files

---
+++ yfs.c
@@ -0,0 +1,12 @@
+#include <comp421/filesystem.h>
+#include <comp421/yalnix.h>
+
+int
+main(int argc, char **argv)
+{
+    (void) argc;
+    (void) argv;
+    TracePrintf(0, "Hello");
+
+    return 0;
+}
